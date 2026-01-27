9cfda1c95bece6d2f43148b60bf566520d46dd4c
Alberto Balaguer <alberto.balaguer184@comunidadunir.netgit config --global user.emailgit config --global user.email alberto.balaguer184@comunidadunir.net>
2026-01-27 19:52:04 +0100

    Primer commit de Angular

---
+++ FrontEnd/tienda-online-frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', loadComponent: () => import('./components/home/home').then(m => m.HomeComponent)},
+    { path: 'about', loadComponent: () => import('./components/about/about').then(m => m.AboutComponent)},
+    { path: 'favoritos', loadComponent: () => import('./components/favorites/favorites').then(m => m.FavoritesComponent)},
+    { path: 'perfil', loadComponent: () => import('./components/profile/profile').then(m => m.ProfileComponent)},
+    { path: 'carrito', loadComponent: () => import('./components/cart/cart').then(m => m.CartComponent)},
+    { path: 'busqueda', loadComponent: () => import('./components/search/search').then(m => m.SearchComponent)},
+    { path: 'categoria/:tipo', loadComponent: () => import('./components/products/products').then(m => m.ProductsComponent)},
+    { path: 'producto/:id', loadComponent: () => import('./components/product-detail/product-detail').then(m => m.ProductDetailComponent)},
+    { path: '**', redirectTo: '/inicio' }
+];
