72be9ca9638c0a39452ffc33864c59f28c3b8e37
Francisco Jose Soria <franciscosorianavarrete@gmail.com>
2026-02-27 11:51:48 +0100

    Back metodo buscar por nombre

--- BackEnd/FJSN_ZGE_ABG_Proyecto_TFC_01/src/main/java/tiendaonline/repository/ProductoRepository.java
+++ BackEnd/FJSN_ZGE_ABG_Proyecto_TFC_01/src/main/java/tiendaonline/repository/ProductoRepository.java
@@ -10,4 +10,7 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     //Filtro productos por nombre de categoria
     List<Producto> findByCategoria_NombreCategoriaIgnoreCase(String nombreCategoria);
 
+    //Busco productos por su nombre
+    List<Producto> findByNombreProductoContainingIgnoreCase(String nombreProducto);
+
 }
